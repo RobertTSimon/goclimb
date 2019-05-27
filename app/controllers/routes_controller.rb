6f56e1a006a80c2554314509d577d3fcda021eab
Simon dHarcourt <simondharcourt@gmail.com>
2019-05-27 17:58:29 -0400

    I begin to do the route controller

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,22 +1,139 @@
 class RoutesController < ApplicationController
+  before_action :set_route, only: [:show, :edit, :destroy, :update]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+
   def new
+    @route = Route.new
+    # @route_attachment = @route.route_attachments.build
+    authorize @route
   end
 
   def create
+    @route = Route.new(route_params)
+    if route.save
+      redirect route_path(@route)
+    else
+      render :new
+    end
   end
 
   def index
+    @routes = Route.all
   end
 
   def show
+    authorize @route
   end
 
   def destroy
+    authorize @route
   end
 
   def edit
+    authorize @route
   end
 
   def update
+  end
+
+  private
+
+  def set_route
+    @route = route.find(params[:id])
+  end
+
+  def route_params
+    route.permit(:route).require(:name, :longitude, :lattitude, :description, :type, :style, :level, :rating)
+  end
+end
+
+
+class EquipmentController < ApplicationController
+  before_action :set_equipment, only: [:show, :edit, :destroy, :update]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+      @routes = policy_scope(route).order(created_at: :desc)
+    # @route = route.all
+    if params[:query].present?
+      @route = route.global_search(params[:query])
+    elsif params[:sport]
+      @route = route.where(sport: params[:sport])
+    else
+      # Show nothing
+      # @route = []
+      # Show all
+      @route = route.all
+    end
+
+    if user_signed_in?
+      @route = @route.reject do |route|
+        route.user.username == current_user.username
+      end
+    end
+  end
+
+
+  def show
+    authorize @route
+    @marker = {
+      lat: @route.latitude,
+      lng: @route.longitude,
+      # infoWindow: render_to_string(partial: "infowindow", locals: { route: @route })
+      image_url: helpers.asset_url('rond.png')
+      }
+    @booking = Booking.new
+    @review = Review.new(route: @route)
+  end
+
+  def create
+    @route = route.new(route_allowed_params)
+    authorize @route
+    @route.user = current_user
+
+    if !params[:route_attachments].nil?
+      params[:route_attachments]['photo'].each do |a|
+         @route.route_attachments.new(:photo => a)
+      end
+    end
+
+    if @route.save
+      redirect_to route_path(@route), notice: "Listing was successfully created!"
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+   authorize @route
+  end
+
+  def update
+    if @route.update(route_allowed_params)
+      if !params[:route_attachments].nil?
+        params[:route_attachments]['photo'].each do |a|
+          @route_attachment = @route.route_attachments.create!(:photo => a, :route_id => @route.id)
+        end
+      end
+      redirect_to route_path(@route), notice: "Listing was successfully updated!"
+    else
+      render :edit
+    end
+    authorize @route
+  end
+
+  def destroy
+    @route.destroy
+    redirect_to route_index_path, notice: "Listing was successfully destroyed!"
+    authorize @route
+  end
+
+  private
+
+
+  def route_allowed_params
+    params.require(:route).permit(:name, :sport, :description, :address, :price, equipment_attachments_attributes: [:id, :equipment_id, :photo])
   end
 end
