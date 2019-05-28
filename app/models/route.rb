60f91a3be8a9f07183ffa9b002b87ae89d140c8a
Robert Simon <robertsimonmedia@gmail.com>
2019-05-28 15:19:00 -0400

    validation of models completed

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,12 @@
 class Route < ApplicationRecord
-  belongs_to :wall
+  belongs_to :site
+
+  validates :name, presence: true, uniqueness: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :description, presence: true
+  validates :type, presence: true
+  validates :style, presence: true
+  validates :level, presence: true
+  validates :rating, presence: true
 end
--- app/models/state_province.rb
+++ app/models/state_province.rb
@@ -1,2 +1,3 @@
 class StateProvince < ApplicationRecord
+  validates :name, presence: true
 end
