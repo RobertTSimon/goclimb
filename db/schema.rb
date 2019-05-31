ba5a0205b263aebccd8310264eda429fe3be1dd9
Javier Madrid <jmadridvaquero@gmail.com>
2019-05-31 12:16:35 -0400

    navbar modified as per specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_190312) do
+ActiveRecord::Schema.define(version: 2019_05_31_145546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_190312) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "state"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
