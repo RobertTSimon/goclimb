216cbc8b4ef938165b47a6ea6c7cae7d9539d056
Diego Ulibarri <diegoulhe@outlook.com>
2019-06-04 13:32:07 -0400

    Add styling to the show page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :route
-  
 
-  validates :title, presence: true
   validates :description, presence: true
-  validates :rating, presence: true
-
-
 end
