fe103975069b9d0e5085a28772040c97035826ea
Simon dHarcourt <simondharcourt@gmail.com>
2019-06-05 10:16:02 -0400

    distance loc + links

--- app/javascript/plugins/init_distance_site.js
+++ app/javascript/plugins/init_distance_site.js
@@ -4,7 +4,6 @@ const siteElement = document.getElementById('distance_site');
 const calculDistanceSite = (marker, geoloc) => {
     const lat1 = geoloc.latitude*2*3.14/360
     const lon1 = geoloc.longitude*2*3.14/360
-    console.log(marker)
     const lat2 = marker.lat*2*3.14/360
     const lon2 = marker.long*2*3.14/360
     return Math.acos(Math.sin(lat1)*Math.sin(lat2)+Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1))*6371;
@@ -12,7 +11,8 @@ const calculDistanceSite = (marker, geoloc) => {
 
  const displaySite = (distance) => {
   if (siteElement) {
-     siteElement.innerHTML = siteElement.innerHTML + `<p>DISTANCE FROM YOU: ${Math.trunc(distance)} km</p>`;
+    console.log(Math.trunc(distance))
+     siteElement.innerHTML = `<p>DISTANCE FROM YOU: ${Math.trunc(distance)} km</p>`;
     }
   };
 const initDistanceSite = () => {
